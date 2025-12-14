2219d774acc1a04ea6c95e88571c7b818d244c84
M Eran Hasareli <eran.hasarali.2001@gmail.com>
2025-12-14 18:38:23 +0530

    update the index file

--- index.php
+++ index.php
@@ -4,23 +4,6 @@
   
   $myArray = ["Hello", "World"];
   
-  var_dump($myArray);
-  echo "<br />";
   print_r($myArray);
 
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>PHP!</title>
-</head>
-
-<body>
-
-  <h1>
-    Hello
-  </h1>
-
-</body>
-</html>
